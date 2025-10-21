aa58438eea38a0bf15c534a8b56ab8cb6a9e4799
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-10-21 22:37:40 +0700

    Add gravity

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ const canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
 const canvasHeight = (canvas.height = window.innerHeight - 10);
 const canvasWidth = (canvas.width = window.innerWidth - 10);
 
+let gravityStrength = 1;
+
 const player = {
+  x: 0,
+  // y: canvasHeight - 64,
+  y: 0, // TODO: Revert to the line above later.
   h: 64,
   w: 64,
-  x: 0,
-  y: canvasHeight - 64,
   c: "black",
+  dy: 0,
+  isGrounded: false,
 };
 
 const obstacle = {
-  h: 64,
-  w: 64,
   x: canvasWidth - 64,
   y: canvasHeight - 64 - 48, // TODO: Randomize between air and ground obstacle.
+  h: 64,
+  w: 64,
   c: "red",
+};
+
+const clearDrawing = () => {
+  canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
 };
 
 const drawPlayer = () => {
@@ -29,5 +38,24 @@ const drawObstacle = () => {
   canvasCtx.fillRect(obstacle.x, obstacle.y, obstacle.w, obstacle.h);
 };
 
-drawPlayer();
-drawObstacle();
+const gravity = () => {
+  if (player.y + player.h + player.dy < canvasHeight) {
+    player.dy += gravityStrength;
+    player.y += player.dy;
+    player.isGrounded = false;
+  } else {
+    player.dy = 0;
+    player.y = canvasHeight - player.h;
+    player.isGrounded = true;
+  }
+};
+
+const animate = () => {
+  gravity();
+  clearDrawing();
+  drawPlayer();
+  drawObstacle();
+  requestAnimationFrame(animate);
+};
+
+requestAnimationFrame(animate);
