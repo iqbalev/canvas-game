091d331317e700ea06e0ccf3eda34628ba57e3b2
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-10-28 21:54:41 +0700

    Add scoring, centralize states, cap and increase speed

--- src/main.ts
+++ src/main.ts
@@ -32,97 +32,143 @@ type Obstacle = {
   color: string;
 };
 
+type Configs = {
+  speed: number;
+  maxSpeed: number;
+  gravityAcceleration: number;
+};
+
+type Stats = {
+  score: number;
+  highScore: number;
+};
+
+type Game = {
+  player: Player;
+  obstacle: Obstacle;
+  configs: Configs;
+  stats: Stats;
+};
+
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
 const canvasHeight: number = (canvas.height = window.innerHeight - 10);
 const canvasWidth: number = (canvas.width = window.innerWidth - 10);
-const gravityAcceleration: number = 2;
 const keys: Keys = {};
 
-let gameSpeed: number = 3;
-
-const player: Player = {
-  xPosition: 64,
-  yPosition: canvasHeight - 64,
-  width: 64,
-  height: 64,
-  originalHeight: 64,
-  color: "black",
-  yVelocity: 0,
-  jumpHoldFrames: 0,
-  jumpVelocity: 16,
-  isGrounded: false,
-};
-
-const obstacle: Obstacle = {
-  xPosition: canvasWidth,
-  yPosition: canvasHeight - 32 - 48, // TODO: Randomize between air and ground obstacle.
-  width: 32,
-  height: 32,
-  color: "maroon",
+const game: Game = {
+  player: {
+    xPosition: 64,
+    yPosition: canvasHeight - 64,
+    width: 64,
+    height: 64,
+    originalHeight: 64,
+    color: "black",
+    yVelocity: 0,
+    jumpHoldFrames: 0,
+    jumpVelocity: 16,
+    isGrounded: true,
+  },
+  obstacle: {
+    // TODO: Randomize between air and ground obstacle.
+    xPosition: canvasWidth,
+    yPosition: canvasHeight - 32,
+    width: 32,
+    height: 32,
+    color: "maroon",
+  },
+  configs: {
+    speed: 5,
+    maxSpeed: 50,
+    gravityAcceleration: 2,
+  },
+  stats: {
+    score: 0,
+    highScore: 0,
+  },
 };
 
 const duck = (): void => {
   if (keys["s"] || keys["Control"] || keys["ArrowDown"]) {
-    if (player.isGrounded) {
-      player.height = player.originalHeight / 2;
-      player.yPosition = canvasHeight - player.height;
+    if (game.player.isGrounded) {
+      game.player.height = game.player.originalHeight / 2;
+      game.player.yPosition = canvasHeight - game.player.height;
     }
   } else {
-    player.height = player.originalHeight;
+    game.player.height = game.player.originalHeight;
   }
 };
 
 const jump = (): void => {
   if (keys[" "] || keys["w"] || keys["ArrowUp"]) {
-    if (player.isGrounded && player.jumpHoldFrames === 0) {
-      player.jumpHoldFrames = 1;
-      player.yVelocity = -player.jumpVelocity;
+    if (game.player.isGrounded && game.player.jumpHoldFrames === 0) {
+      game.player.jumpHoldFrames = 1;
+      game.player.yVelocity = -game.player.jumpVelocity;
     } else if (
-      player.jumpHoldFrames > 0 &&
-      player.jumpHoldFrames <= player.jumpVelocity
+      game.player.jumpHoldFrames > 0 &&
+      game.player.jumpHoldFrames <= game.player.jumpVelocity
     ) {
-      player.jumpHoldFrames++;
-      player.yVelocity = -player.jumpVelocity - player.jumpHoldFrames / 75;
+      game.player.jumpHoldFrames++;
+      game.player.yVelocity =
+        -game.player.jumpVelocity - game.player.jumpHoldFrames / 75;
     }
   } else {
-    player.jumpHoldFrames = 0;
+    game.player.jumpHoldFrames = 0;
   }
 };
 
 const gravity = (): void => {
-  if (player.yPosition + player.height + player.yVelocity < canvasHeight) {
-    player.yVelocity += gravityAcceleration;
-    player.yPosition += player.yVelocity;
-    player.isGrounded = false;
+  if (
+    game.player.yPosition + game.player.height + game.player.yVelocity <
+    canvasHeight
+  ) {
+    game.player.yVelocity += game.configs.gravityAcceleration;
+    game.player.yPosition += game.player.yVelocity;
+    game.player.isGrounded = false;
   } else {
-    player.yVelocity = 0;
-    player.yPosition = canvasHeight - player.height;
-    player.isGrounded = true;
+    game.player.yVelocity = 0;
+    game.player.yPosition = canvasHeight - game.player.height;
+    game.player.isGrounded = true;
   }
 };
 
 const isPlayerCollideWithObstacle = (): boolean => {
-  if (
-    player.xPosition + player.width >= obstacle.xPosition &&
-    player.xPosition <= obstacle.xPosition + obstacle.width &&
-    player.yPosition + player.height >= obstacle.yPosition &&
-    player.yPosition <= obstacle.yPosition + obstacle.height
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  return (
+    game.player.xPosition + game.player.width >= game.obstacle.xPosition &&
+    game.player.xPosition <= game.obstacle.xPosition + game.obstacle.width &&
+    game.player.yPosition + game.player.height >= game.obstacle.yPosition &&
+    game.player.yPosition <= game.obstacle.yPosition + game.obstacle.height
+  );
 };
 
-const spawnObstacle = (): void => {
-  obstacle.xPosition -= gameSpeed;
+const isObstacleOffScreen = (): boolean => {
+  return (
+    game.obstacle.xPosition + game.obstacle.width < canvasWidth - canvasWidth
+  );
+};
 
-  if (
-    isPlayerCollideWithObstacle() ||
-    obstacle.xPosition + obstacle.width < canvasWidth - canvasWidth
-  ) {
-    obstacle.xPosition = canvasWidth;
+const updateGame = (): void => {
+  game.obstacle.xPosition -= game.configs.speed;
+  game.stats.score++;
+
+  if (game.configs.speed < game.configs.maxSpeed) {
+    game.configs.speed += 0.001;
+  } else {
+    game.configs.speed = game.configs.maxSpeed;
+  }
+
+  if (isPlayerCollideWithObstacle()) {
+    if (game.stats.score > game.stats.highScore) {
+      game.stats.highScore = game.stats.score;
+    }
+
+    game.obstacle.xPosition = canvasWidth;
+    game.stats.score = 0;
+    game.configs.speed = 5;
+  }
+
+  if (isObstacleOffScreen()) {
+    game.obstacle.xPosition = canvasWidth;
   }
 };
 
@@ -130,38 +176,48 @@ const clearDrawing = (): void => {
   canvasCtx.clearRect(0, 0, canvasWidth, canvasHeight);
 };
 
+const drawScoreboard = (): void => {
+  canvasCtx.fillStyle = "black";
+  canvasCtx.font = "1.5rem monospace";
+  canvasCtx.fillText(`Score: ${game.stats.score}`, 64, 64);
+  canvasCtx.fillText(
+    `High Score: ${game.stats.highScore}`,
+    canvasWidth - 256,
+    64
+  );
+};
+
 const drawPlayer = (): void => {
-  canvasCtx.fillStyle = player.color;
+  canvasCtx.fillStyle = game.player.color;
   canvasCtx.fillRect(
-    player.xPosition,
-    player.yPosition,
-    player.width,
-    player.height
+    game.player.xPosition,
+    game.player.yPosition,
+    game.player.width,
+    game.player.height
   );
 };
 
 const drawObstacle = (): void => {
-  canvasCtx.fillStyle = obstacle.color;
+  canvasCtx.fillStyle = game.obstacle.color;
   canvasCtx.fillRect(
-    obstacle.xPosition,
-    obstacle.yPosition,
-    obstacle.width,
-    obstacle.height
+    game.obstacle.xPosition,
+    game.obstacle.yPosition,
+    game.obstacle.width,
+    game.obstacle.height
   );
 };
 
-const start = (): void => {
+const gameLoop = (): void => {
   duck();
   jump();
   gravity();
-  spawnObstacle();
+  updateGame();
   clearDrawing();
+  drawScoreboard();
   drawPlayer();
   drawObstacle();
-  requestAnimationFrame(start);
+  requestAnimationFrame(gameLoop);
 };
-
-requestAnimationFrame(start);
 
 document.addEventListener("keydown", (e) => {
   keys[e.key as keyof Keys] = true;
@@ -170,3 +226,5 @@ document.addEventListener("keydown", (e) => {
 document.addEventListener("keyup", (e) => {
   keys[e.key as keyof Keys] = false;
 });
+
+requestAnimationFrame(gameLoop);
